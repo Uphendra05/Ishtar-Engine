419a8858b309e1f748ec3e92cc49d1d6acc1f2f5
Uphendhra <altairuphi0523@gmail.com>
2025-06-12 16:34:14 -0400

    Buttons Setup in Content Browser

--- Project1/src/ImGui/ContentBrowser.cpp
+++ Project1/src/ImGui/ContentBrowser.cpp
@@ -1,7 +1,19 @@
 #include "ContentBrowser.h"
 
+
+namespace fs = std::filesystem;
+
+static const std::filesystem::path sAssetPath = "../Project1";
+
+ContentBrowser::ContentBrowser() : mCurrentDirectory(sAssetPath)
+{
+
+
+}
+
 void ContentBrowser::OnRender(float windowWidth, float windowHeight)
 {
+
 
     // isEnable = false;
     if (!isPanelOpen)
@@ -56,14 +68,73 @@ void ContentBrowser::OnRender(float windowWidth, float windowHeight)
     }
 
     renderPos.y += lineHeight;
-
     renderPos.x *= -0.0005f;
 
-   
+
+
+    if (mCurrentDirectory != std::filesystem::path(sAssetPath))
+    {
+        if (ImGui::Button("<-"))
+        {
+            mCurrentDirectory = mCurrentDirectory.parent_path();
+        }
+    }
+
+    static float paddingThumbSize = 16.0f;
+    static float thumbnailSize = 256;
+    float cellSize = paddingThumbSize + thumbnailSize;
+    float pandelWidth = ImGui::GetContentRegionAvail().x;
+    int columnCount = (int) (pandelWidth / cellSize);
+    if (columnCount < 1) columnCount = 1;
+
+    ImGui::Columns(columnCount, 0, false);
+
+    for (auto& directoryEntry : fs::directory_iterator(mCurrentDirectory))
+    {
+
+        const fs::path& path = directoryEntry.path();
+        auto relativePath =  std::filesystem::relative(path, sAssetPath);
+        std::string displayName = relativePath.filename().string();
+
+        if (mCurrentDirectory == sAssetPath && !directoryEntry.is_directory())
+            continue; 
+
+
+        ImGui::Button(displayName.c_str(), { thumbnailSize,thumbnailSize });
+        ImGui::Text(displayName.c_str());
+
+
+
+        if (directoryEntry.is_directory())
+        {
+           /* if (ImGui::Button(displayName.c_str()))
+            {
+                mCurrentDirectory /= path.filename();
+            }*/
+        }
+        else
+        {
+            /*if (ImGui::Button(displayName.c_str()))
+            {
+
+            }*/
+
+        }
+        
+        ImGui::NextColumn();
+
+    }
+
+
+
+    ImGui::Columns(1);
+
+    ImGui::SliderFloat("Thumbnail Size", &thumbnailSize, 16, 512);
+    ImGui::SliderFloat("Padding", &paddingThumbSize, 0, 32);
+
 
     ImGui::End();
 
     ImGui::PopStyleColor();
     ImGui::PopStyleVar();
-
 }
--- Project1/src/ImGui/ContentBrowser.h
+++ Project1/src/ImGui/ContentBrowser.h
@@ -1,13 +1,21 @@
 #pragma once
 #include "BasePanel.h"
+#include <iostream>
+#include <filesystem>
+#include <fstream>
 
 class ContentBrowser : public BasePanel
 {
-
+public:
+	ContentBrowser();
 	// Inherited via BasePanel
 	void OnRender(float windowWidth, float windowHeight) override;
 	ImVec2 viewPortRes;
 	ImVec2 viewPortSize;
 
 	ImVec4 windowColor = ImVec4(0.21f, 0.1f, 0.1f, 1.0f);
-};
+
+private:
+
+	std::filesystem::path mCurrentDirectory ;
+}; 
